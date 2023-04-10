74b31c2316e1a421dec069826c2b4d57d09bf887
Juan Gabriel <juangacilin@gmail.com>
2023-04-10 17:16:37 -0300

    Colocando pecas no tabuleiro

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using xadrez;
 using tabuleiro;
 using Xadrez_Console;
 
@@ -9,7 +9,9 @@ namespace XadrezConsole
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
-
+            tab.colocarPeca(new Torre(Cor.Preta,tab), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
             Tela.imprimirTabuleiro(tab);
             
                 Console.WriteLine();
